dec6078495fd540370d7c781bbe4ecca1612a403
lucasprus <lucas.prus@gmail.com>
2016-10-11 14:48:55 +0200

    Project setup

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name contentfulCustomCmsApp
+ * @description
+ * # contentfulCustomCmsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('contentfulCustomCmsApp', [
+    'ngAnimate',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name contentfulCustomCmsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the contentfulCustomCmsApp
+ */
+angular.module('contentfulCustomCmsApp')
+  .controller('MainCtrl', function() {
+      this.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+      ];
+  });
